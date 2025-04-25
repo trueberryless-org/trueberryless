8bf6f5f16831a7656bf3897f641e127c842a2c79
trueberryless <trueberryless+github@gmail.com>
2025-04-25 17:04:44 +0200

    feat: image sections

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,10 +2,12 @@
 import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     responsiveImages: true,
   },
-  integrations: [expressiveCode()],
+  integrations: [expressiveCode(), mdx()],
 });
