1e87d327d35fda53b0101b2bbdef8226678f6d0a
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-10-01 22:01:57 +0200

    feat: add Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: netlify(),
 });
