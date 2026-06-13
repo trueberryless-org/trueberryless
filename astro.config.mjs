5afec5cd8ffaf94390a6dd5d9d0e73c7940c0af0
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-06-13 23:17:11 +0200

    feat: seo, robots, og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import netlify from "@astrojs/netlify";
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
     process.env.ADAPTER_TYPE === "netlify"
       ? netlify()
       : node({ mode: "standalone" }),
+  integrations: [sitemap()],
+  prefetch: {
+    defaultStrategy: "hover",
+    prefetchAll: true,
+  },
 });
